861abf92da35302379a1e80db3dfbdb09ad380e5
Alexander Klokov <alexander.klokov@gmail.com>
2020-04-15 21:42:58 -0500

    render icons on hover

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import {useState} from 'react'
 
-export const Image = ({className, img}) => (
-  <div className={`${className} image-container`}>
+export const Image = ({className, img}) => {
+
+  const [isHovered, setIsHovered] = useState(false)
+
+  const maybeHeartIcon = isHovered && <i className="ri-heart-line favorite"></i>
+  const maybeCartIcon = isHovered && <i className="ri-add-circle-line cart"></i>
+
+
+  return (
+  <div className={`${className} image-container`}
+    onMouseEnter={() => setIsHovered(true)}
+    onMouseLeave={() => setIsHovered(false)}>
+
     <img src={img.url} className="image-grid"/>
+
+    {maybeHeartIcon}
+
+    {maybeCartIcon}
+
   </div>
-)
+  )
+}
