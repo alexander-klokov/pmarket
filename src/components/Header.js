e05c9801757c4c8103ac8dcf183346864f13a48c
Alexander Klokov <alexander.klokov@gmail.com>
2020-04-16 21:17:00 -0500

    change cart icon if something is in the cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 import React from "react"
+import {useContext} from "react"
+
 import {Link} from "react-router-dom"
 
-export const Header = () => (
+import {Context} from "../Context"
+
+export const Header = () => {
+  const {photoIdsInCart} = useContext(Context)
+
+  const isCartEmpty = photoIdsInCart.length === 0
+  const cartIconType = isCartEmpty ? "ri-shopping-cart-line" : "ri-shopping-cart-fill"
+
+  return (
     <header>
       <Link to="/"><h2>PMarket</h2></Link>
-      <Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-2x"></i></Link>
+      <Link to="/cart"><i className={`${cartIconType} ri-fw ri-2x`}></i></Link>
     </header>
-)
+  )
+}
